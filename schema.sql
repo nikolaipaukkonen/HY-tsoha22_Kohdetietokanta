b9c41682391bc2ec67fe7271af2b6168db0d7d28
nikolaipaukkonen <nikolai.paukkonen@helsinki.fi>
2022-07-29 23:56:16 +0300

    create db schema

---
+++ schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE locations (id SERIAL PRIMARY KEY, name TEXT UNIQUE, dating_id INTEGER REFERENCES datings, type_id REFERENCES types); 
+CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT UNIQUE, password TEXT);
+CREATE TABLE comments (id SERIAL PRIMARY KEY, content TEXT, user_id INTEGER REFERENCES users, sent_at TIMESTAMP, location_id INTEGER REFERENCES locations);
+CREATE TABLE datings (id SERIAL PRIMARY KEY, dating TEXT UNIQUE);
+CREATE TABLE types (id SERIAL PRIMARY KEY, type_name TEXT UNIQUE);
+CREATE TABLE coordinates (id SERIAL PRIMARY KEY, location_id INTEGER REFERENCES locations, x FLOAT8, y FLOAT8, z FLOAT8);
+
